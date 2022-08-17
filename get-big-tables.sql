4903bfdeb667ed60f3c118a4ee416c84817b74e8
ibrahim Ceyişakar <ibrahimceyisakar@ibrahims-MacBook-Air.local>
2022-08-17 06:21:32 +0300

    first commit, added usefull sql for showing big tables in database in a nice format

---
+++ get-big-tables.sql
@@ -0,0 +1,22 @@
+
+/* 
+    List all tables in the database, ordered by size, descending, use text format.
+*/
+
+SELECT
+  schema_name,
+  relname,
+  pg_size_pretty(table_size) AS size,
+  table_size
+
+FROM (
+       SELECT
+         pg_catalog.pg_namespace.nspname           AS schema_name,
+         relname,
+         pg_relation_size(pg_catalog.pg_class.oid) AS table_size
+
+       FROM pg_catalog.pg_class
+         JOIN pg_catalog.pg_namespace ON relnamespace = pg_catalog.pg_namespace.oid
+     ) t
+WHERE schema_name NOT LIKE 'pg_%'
+ORDER BY table_size DESC;
